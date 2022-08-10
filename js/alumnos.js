6b65fef3b17dcd0717e240fee6ce4d71bcf36490
lucas <lucasclemente2016@gmail.com>
2022-08-10 19:43:57 -0300

    visuales

--- js/alumnos.js
+++ js/alumnos.js
@@ -1,148 +1,176 @@
 let i = 0;
 let form = document.getElementById("form");
-let tabla=document.getElementById("alumnos-check");
+let tabla = document.getElementById("alumnos-check");
 
-function ingresoDatos() {    
-    form.addEventListener("submit", function(e) {
-       e.preventDefault();
-    
-       const nombre = document.getElementById("nombre").value;
-       const ed = document.getElementById("edad").value;
-       const eml = document.getElementById("email").value;
-         const ps = document.getElementById("pais").value;
-         const zp = document.getElementById("zip").value;
-         const usme = document.getElementById("username").value
-         const dir = document.getElementById("direccion").value
-       const mat = document.getElementById("materias").value
-       const p1 = document.getElementById("parcial1").value;
-      const p2 = document.getElementById("parcial2").value;
-    
+function ingresoDatos() {
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
 
-  // añadirAlumno()
-    
-  });
-  
- 
+        const nombre = document
+            .getElementById("nombre")
+            .value;
+        const ed = document
+            .getElementById("edad")
+            .value;
+        const eml = document
+            .getElementById("email")
+            .value;
+        const ps = document
+            .getElementById("pais")
+            .value;
+        const zp = document
+            .getElementById("zip")
+            .value;
+        const usme = document
+            .getElementById("username")
+            .value
+        const dir = document
+            .getElementById("direccion")
+            .value
+        const mat = document
+            .getElementById("materias")
+            .value
+        const p1 = document
+            .getElementById("parcial1")
+            .value;
+        const p2 = document
+            .getElementById("parcial2")
+            .value;
+
+        // añadirAlumno()
+
+    });
+
 }
 // enviar al local storage
 
-    
-form.addEventListener("submit", (e)=> {
-   e.preventDefault();
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-   const nombre = document.getElementById("nombre").value;
-   const ed = document.getElementById("edad").value;
-   const eml = document.getElementById("email").value;
-     const ps = document.getElementById("pais").value;
-     const zp = document.getElementById("zip").value;
-     const usme = document.getElementById("username").value
-     const dir = document.getElementById("direccion").value
-   const mat = document.getElementById("materias").value
-   const p1 = document.getElementById("parcial1").value;
-  const p2 = document.getElementById("parcial2").value;
+    const nombre = document
+        .getElementById("nombre")
+        .value;
+    const ed = document
+        .getElementById("edad")
+        .value;
+    const eml = document
+        .getElementById("email")
+        .value;
+    const ps = document
+        .getElementById("pais")
+        .value;
+    const zp = document
+        .getElementById("zip")
+        .value;
+    const usme = document
+        .getElementById("username")
+        .value
+    const dir = document
+        .getElementById("direccion")
+        .value
+    const mat = document
+        .getElementById("materias")
+        .value
+    const p1 = document
+        .getElementById("parcial1")
+        .value;
+    const p2 = document
+        .getElementById("parcial2")
+        .value;
 
-  //obtener valores de los inputs
-nombre.value;
-ed.value;
-eml.value;
-ps.value;
-zp.value;
-usme.value;
-dir.value;
-mat.value;
-p1.value;
-p2.value;
+    //obtener valores de los inputs
+    nombre.value;
+    ed.value;
+    eml.value;
+    ps.value;
+    zp.value;
+    usme.value;
+    dir.value;
+    mat.value;
+    p1.value;
+    p2.value;
 
-//localStorage
-function añadirAlumno() {
-localStorage.setItem("nombre", nombre); 
-localStorage.setItem("edad", ed);
-localStorage.setItem("email", eml);
-localStorage.setItem("pais", ps);
-localStorage.setItem("zip", zp);
-localStorage.setItem("username", usme);
-localStorage.setItem("direccion", dir);
-localStorage.setItem("materias", mat);
-localStorage.setItem("parcial1", p1);
-localStorage.setItem("parcial2", p2);
-}
+    //localStorage
+    function añadirAlumno() {
+        localStorage.setItem("nombre", nombre);
+        localStorage.setItem("edad", ed);
+        localStorage.setItem("email", eml);
+        localStorage.setItem("pais", ps);
+        localStorage.setItem("zip", zp);
+        localStorage.setItem("username", usme);
+        localStorage.setItem("direccion", dir);
+        localStorage.setItem("materias", mat);
+        localStorage.setItem("parcial1", p1);
+        localStorage.setItem("parcial2", p2);
+    }
 
-function mostrarLocalStorage(){
-    let nombre = localStorage.getItem("nombre");
-    let edad = localStorage.getItem("edad");
-    let email = localStorage.getItem("email");
-    let pais = localStorage.getItem("pais");
-    let zip = localStorage.getItem("zip");
-    let username = localStorage.getItem("username");
-    let direccion = localStorage.getItem("direccion");
-    let materias = localStorage.getItem("materias");
-    let parcial1 = localStorage.getItem("parcial1");
-    let parcial2 = localStorage.getItem("parcial2");
+    function mostrarLocalStorage() {
+        let nombre = localStorage.getItem("nombre");
+        let edad = localStorage.getItem("edad");
+        let email = localStorage.getItem("email");
+        let pais = localStorage.getItem("pais");
+        let zip = localStorage.getItem("zip");
+        let username = localStorage.getItem("username");
+        let direccion = localStorage.getItem("direccion");
+        let materias = localStorage.getItem("materias");
+        let parcial1 = localStorage.getItem("parcial1");
+        let parcial2 = localStorage.getItem("parcial2");
 
-    let promedioParcial = (parseInt(parcial1) + parseInt(parcial2)+parseInt(materias))/3;
-    let container = document.createElement("tr");
-    container.innerHTML = `
+        let promedioParcial = (
+            parseInt(parcial1) + parseInt(parcial2) + parseInt(materias)
+        ) / 3;
+        let container = document.createElement("tr");
+        container.innerHTML = `
     <td>${nombre}</td>
     <td>${edad}</td>
     <td>${username}</td>
     <td>${email}</td>
     <td>${pais}</td>
     <td>${direccion}</td>
-    <td>${parseInt(promedioParcial)}</td>
+    <td>${parseInt(
+            promedioParcial
+        )}</td>
     `
-    tabla.appendChild(container);
-}
+        tabla.appendChild(container);
+    }
 
-añadirAlumno();
-mostrarLocalStorage();
-
+    añadirAlumno();
+    mostrarLocalStorage();
 
 });
 
+function verEstadisticas() {
+    let cont = 0;
+    let promedioParcial = 0;
+    let acumMaterias = 0;
+    let mayor = 0;
+    let nomMayor = "";
 
+    for (let i = 0; i < edad.length; i++) {
 
+        let ed = edad[i];
+        let nom = nombre[i];
+        let mat = materias[i];
+        let p1 = parcial1[i];
+        let p2 = parcial2[i];
 
+        cont++;
 
-// function verEstadisticas() {
-//     let cont=0;
-//     let promedioParcial = 0;
-//     let acumMaterias = 0;
-//     let mayor = 0;
-//     let nomMayor = "";
- 
-//     for (let i = 0; i < edad.length; i++) {
- 
- 
-//        let ed=edad[i];
-//        let nom = nombre[i];
-//        let mat = materias[i];
-//        let p1=parcial1[i];
-//        let p2=parcial2[i];
- 
- 
- 
-//        cont++;
- 
-//        promedioParcial = (p1 + p2) / 2;
-//         acumMaterias = acumMaterias + mat;
- 
-//        if (ed>=18) {
-//           mayor++;
-//           nomMayor=nom;
-//        }
-//     }
- 
-//     const _p = document.querySelector("p");
-//     _p.innerHTML = `
-//        El numero de estudiantes es: ${cont};
-//        El numero de estudiantes mayores de edad es: ${mayor}
-//        El nombre del estudiante mayor de edad es: ${nomMayor}
-//        El promedio de notas de los parciales es: ${promedioParcial}
-//        El numero de materias que cursa es: ${acumMaterias}
-//         `;
-//  }
+        promedioParcial = (p1 + p2) / 2;
+        acumMaterias += mat;
 
- 
+        if (ed >= 18) {
+            mayor++;
+            nomMayor = nom;
+        }
+    }
 
-
-// verEstadisticas()
+    const _p = document.querySelector("p");
+    _p.innerHTML = `
+       El numero de estudiantes es: ${cont};
+       El numero de estudiantes mayores de edad es: ${mayor}
+       El nombre del estudiante mayor de edad es: ${nomMayor}
+       El promedio de notas de los parciales es: ${promedioParcial}
+       El numero de materias que cursa es: ${acumMaterias}
+        `;
+}
--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-
-let profesor=["Jorge","Juan","Pedro","Maria","Jose"];
-//eliminar algun profesor
-const deleteProf=(profesor)=>{
-   let index = profesor.indexOf(profesor);
-
-   if(index!=-1){
-      profesor.slice(index,1);
-   }
-}
-
-
 // Objetos
 
 class estudiante {
@@ -48,26 +36,15 @@ class estudiante {
 }
 
 
-
-
-
-
-
-
-
-
-//Ventas de cursos de la universidad 
-// quiero añadir un parte de ventas de cursos para poder tener un poco mas de juego con el codigo
-
- 
+//  validacion de llenado de formulario correctamente 
 (function () {
    'use strict'
  
    window.addEventListener('load', function () {
-     // Fetch all the forms we want to apply custom Bootstrap validation styles to
+ 
      var forms = document.getElementsByClassName('needs-validation')
  
-     // Loop over them and prevent submission
+    
      Array.prototype.filter.call(forms, function (form) {
        form.addEventListener('submit', function (event) {
          if (form.checkValidity() === false) {
